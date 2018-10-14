61ca60fdd7368fe80e2553f667709af1c127a285
ivallee <isaacvallee@gmail.com>
2018-10-14 16:16:27 -0700

    initial setup

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+const Home = props => (
+  <div>
+    <p>Hey there! Again</p>
+  </div>
+);
+
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+const Sell = props => (
+  <div>
+    <p>Sell stuff</p>
+  </div>
+);
+
+export default Sell;
