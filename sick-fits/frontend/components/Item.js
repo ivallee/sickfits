1dc0d9ce3c3855b754dbc5a195299a1afad7d0b9
ivallee <isaacvallee@gmail.com>
2018-10-22 20:58:58 -0700

    build createItem component

--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -15,7 +15,7 @@ class Item extends Component {
     const { item } = this.props;
     return (
       <ItemStyles>
-        {item.image && <img src={item.image} alt={img.title} />}
+        {item.image && <img src={item.image} alt={item.title} />}
         <Title>
           <Link href={{
             pathname: '/item',
