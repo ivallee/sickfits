211eb22b7ebfc69d87653ef2b71ffcd83b8b8ac9
ivallee <isaacvallee@gmail.com>
2018-10-23 21:00:41 -0700

    add updateItem mutation and page

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -11,8 +11,22 @@ const Mutations = {
       info
     );
     return item;
-  }
+  },
+  updateItem(parent, args, ctx, info) {
 
+    const updates = { ...args };
+    delete updates.id;
+
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id,
+        },
+      },
+      info
+    );
+  },
 };
 
 module.exports = Mutations;
