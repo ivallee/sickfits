a967f11b1e6b4a5711af83d8d084385700fc99cb
ivallee <isaacvallee@gmail.com>
2018-10-24 17:01:34 -0700

    extract button component

--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -5,6 +5,7 @@ import ItemStyles from './styles/ItemStyles';
 import Title from './styles/Title';
 import PriceTag from './styles/PriceTag';
 import formatMoney from '../lib/formatMoney';
+import DeleteItem from './DeleteItem';
 
 class Item extends Component {
   static propTypes = {
@@ -36,7 +37,7 @@ class Item extends Component {
             <a>Edit ✏️</a>
           </Link>
           <button>Add To Cart</button>
-          <button>Delete</button>
+          <DeleteItem>Delete</DeleteItem>
         </div>
       </ItemStyles>
     );
