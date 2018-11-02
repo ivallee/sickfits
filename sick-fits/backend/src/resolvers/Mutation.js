86ab28c4715a8d462c967824ee06add0839637f4
ivallee <isaacvallee@gmail.com>
2018-11-01 17:32:16 -0700

    initial reset request

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { randomBytes } = require('crypto');
+const { promisify } = require('util');
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
@@ -80,6 +82,26 @@ const Mutations = {
   signout(parent, args, ctx, info) {
     ctx.response.clearCookie('token');
     return { message: 'Goodbye!' };
+  },
+  async requestReset(parent, { email }, ctx, info) {
+
+    const user = await ctx.db.query.user({ where: { email } });
+    if (!user) {
+      throw new Error(`No user found for email ${email}`);
+    }
+
+    const randomBytesPromisified = promisify(randomBytes);
+    const resetToken = (await randomBytesPromisified(10)).toString('hex');
+    const resetTokenExpiry = Date.now() + 3600000;
+
+    const res = await ctx.db.mutation.updateUser(
+      {
+        where: { email },
+        data: { resetToken, resetTokenExpiry },
+      },
+    );
+    console.log(res);
+    return { message: 'Success!'};
   },
 };
 
