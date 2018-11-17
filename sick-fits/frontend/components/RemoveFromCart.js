a94f092cad320255cf8c482adea2e430f7593bc4
ivallee <isaacvallee@gmail.com>
2018-11-16 18:09:29 -0800

    add delete button

---
+++ sick-fits/frontend/components/RemoveFromCart.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import gql from 'graphql-tag';
+import { CURRENT_USER_QUERY } from './User';
+
+const BigButton = styled.button`
+  font-size: 3rem;
+  background: none;
+  border: 0;
+  &:hover {
+    color: ${props => props.theme.red};
+    cursor: pointer;
+  }
+`;
+
+class RemoveFromCart extends Component {
+  static propTypes = {
+    id: PropTypes.string.isRequired
+  };
+  render() {
+    return (
+      <BigButton>
+        &times;
+      </BigButton>
+    );
+  }
+}
+
+export default RemoveFromCart;
