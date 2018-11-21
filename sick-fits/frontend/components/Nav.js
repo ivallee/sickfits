6d159c243df4d11499c0daa7024ac5b30e012973
ivallee <isaacvallee@gmail.com>
2018-11-20 17:52:06 -0800

    add cart counter and animations

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -4,6 +4,7 @@ import { TOGGLE_CART_MUTATION } from './Cart';
 import NavStyles from './styles/NavStyles';
 import User from'./User';
 import Signout from './Signout';
+import CartCount from './CartCount';
 
 const Nav = () => {
   return (
@@ -27,8 +28,10 @@ const Nav = () => {
               <Signout />
               <Mutation mutation={TOGGLE_CART_MUTATION}>
                 {(toggleCart) => (
-                  // TODO: GET THIS WORKING
-                  <button onClick={toggleCart}>My Cart</button>
+                  <button onClick={toggleCart}>
+                    My Cart
+                    <CartCount count={me.cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0)} />
+                  </button>
                 )}
               </Mutation>
             </>
