05ba9b6fd6fdc76b25780633c1822e3c81fdfff4
ivallee <isaacvallee@gmail.com>
2018-11-20 18:11:59 -0800

    add ability to delete item existing in a cart

--- sick-fits/frontend/components/CartItem.js
+++ sick-fits/frontend/components/CartItem.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import RemoveFromCart from './RemoveFromCart';
 import { timeout, times } from 'async';
+import CartStyles from './styles/CartStyles';
 
 const CartItemStyles = styled.li`
   padding: 1rem 0;
@@ -20,22 +21,30 @@ const CartItemStyles = styled.li`
 
 `;
 
-const CartItem = ({ cartItem }) => (
-  <CartItemStyles>
-    <img width="100" src={cartItem.item.image} alt={cartItem.item.title} />
-    <div className="cart-item-details">
-      <h3>{cartItem.item.title}</h3>
-      <p>
-        {formatMoney(cartItem.item.price * cartItem.quantity)}
-        {' - '}
-        <em>
-          {cartItem.quantity} &times; {formatMoney(cartItem.item.price)} each
-        </em>
-      </p>
-    </div>
-    <RemoveFromCart id={cartItem.id} />
-  </CartItemStyles>
-);
+const CartItem = ({ cartItem }) => {
+  if (!cartItem.item) return (
+    <CartItemStyles>
+      <p>This item was removed</p>
+      <RemoveFromCart id={cartItem.id} />
+    </CartItemStyles>
+  );
+  return (
+    <CartItemStyles>
+      <img width="100" src={cartItem.item.image} alt={cartItem.item.title} />
+      <div className="cart-item-details">
+        <h3>{cartItem.item.title}</h3>
+        <p>
+          {formatMoney(cartItem.item.price * cartItem.quantity)}
+          {' - '}
+          <em>
+            {cartItem.quantity} &times; {formatMoney(cartItem.item.price)} each
+          </em>
+        </p>
+      </div>
+      <RemoveFromCart id={cartItem.id} />
+    </CartItemStyles>
+  );
+}
 
 CartItem.propTypes = {
   cartItem: PropTypes.object.isRequired,
