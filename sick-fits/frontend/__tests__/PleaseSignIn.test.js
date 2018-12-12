1749b7a401976c7b6a8a3301075311e122243024
ivallee <isaacvallee@gmail.com>
2018-12-11 17:34:24 -0800

    tests for PleaseSignIn component

---
+++ sick-fits/frontend/__tests__/PleaseSignIn.test.js
@@ -0,0 +1,53 @@
+import { mount } from 'enzyme';
+import toJSON from 'enzyme-to-json';
+import wait from 'waait';
+import PleaseSignIn from '../components/PleaseSignIn';
+import { CURRENT_USER_QUERY } from '../components/User';
+import { MockedProvider } from 'react-apollo/test-utils';
+import { fakeItem, fakeUser } from '../lib/testUtils';
+
+const notSignedInMocks = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { me: null } },
+  }
+];
+
+const signedInMocks = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { me: fakeUser() } },
+  }
+];
+
+describe('<PleaseSignIn/>', () => {
+  it('renders the signin dialog if not logged in', async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={notSignedInMocks}>
+        <PleaseSignIn />
+      </MockedProvider>
+    );
+    expect(wrapper.text()).toContain('Loading...');
+    await wait();
+    wrapper.update();
+    expect(wrapper.text()).toContain('Sign into your account')
+    expect(wrapper.find('Signin').exists()).toBe(true)
+  });
+
+  it('renders the child component if logged in', async () => {
+    const TestChild = () => <p>Test child</p>
+    const wrapper = mount(
+      <MockedProvider mocks={signedInMocks}>
+        <PleaseSignIn>
+          <TestChild />
+        </PleaseSignIn>
+      </MockedProvider>
+    );
+
+    await wait();
+    wrapper.update();
+    
+    expect(wrapper.contains(<TestChild />)).toBe(true);
+
+  });
+});
