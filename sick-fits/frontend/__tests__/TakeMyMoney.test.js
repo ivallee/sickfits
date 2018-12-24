55cc43941c9124761114d435b6cf910ec38139a3
ivallee <isaacvallee@gmail.com>
2018-12-24 12:32:06 -0800

    tests for TakeMyMoney Component

--- sick-fits/frontend/__tests__/TakeMyMoney.test.js
+++ sick-fits/frontend/__tests__/TakeMyMoney.test.js
@@ -4,7 +4,6 @@ import toJSON from 'enzyme-to-json';
 import NProgress from 'nprogress';
 import Router from 'next/router';
 import { MockedProvider } from 'react-apollo/test-utils';
-import { ApolloConsumer } from 'react-apollo';
 import TakeMyMoney, { CREATE_ORDER_MUTATION } from '../components/TakeMyMoney';
 import { CURRENT_USER_QUERY } from '../components/User';
 import { fakeUser, fakeCartItem } from '../lib/testUtils';
@@ -48,6 +47,50 @@ describe('<TakeMyMoney/>', () => {
       </MockedProvider>
     );
     const component = wrapper.find('TakeMyMoney').instance();
-    console.log(component);
+    component.onToken({ id: 'abc123' }, createOrderMock);
+    expect(createOrderMock).toHaveBeenCalled();
+    expect(createOrderMock).toHaveBeenCalledWith({ variables: { token: 'abc123' } });
+  });
+
+  it('executes the progress bar', async () => {
+    const createOrderMock = jest.fn().mockResolvedValue({
+      data: { createOrder: { id: 'xyz789' } },
+    });
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <TakeMyMoney />
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    NProgress.start = jest.fn();
+    const component = wrapper.find('TakeMyMoney').instance();
+    component.onToken({ id: 'abc123' }, createOrderMock);
+    expect(NProgress.start).toHaveBeenCalled();
+
+  });
+
+  it('routes to order page when completed', async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <TakeMyMoney />
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    const createOrderMock = jest.fn().mockResolvedValue({
+      data: { createOrder: { id: 'xyz789' } },
+    });
+    const component = wrapper.find('TakeMyMoney').instance();
+    Router.router.push = jest.fn();
+    component.onToken({ id: 'abc123' }, createOrderMock);
+    await wait();
+    
+    expect(Router.router.push).toHaveBeenCalledWith({
+      pathname: '/order',
+      query: { 
+        id: 'xyz789' 
+      },
+    });
   });
 });
